4c107f1bd46adc4e6235df4bb3d8865dfd3a070c
Nji Daniel <60320246+Daniel-TheProgrammer@users.noreply.github.com>
2020-10-12 06:33:52 +0100

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -5,7 +5,7 @@ import shrinkImageAsync from './utils/shrinkImageAsync';
 import uploadPhoto from './utils/uploadPhoto';
 
 const firebase = require('firebase');
-// Required for side-eff
+// Required for side-e
 require('firebase/firestore');
 
 const collectionName = 'snack-SJucFknGX';
